3dafe8acb69ca4fd60ad4e0e67e4f7e53985b46c
raoulvdberge <raoulvdberge@gmail.com>
2024-06-16 11:06:07 +0200

    initial commit

---
+++ src/main/java/com/refinedmods/refinedstorage/curios/ModInitializer.java
@@ -0,0 +1,29 @@
+package com.refinedmods.refinedstorage.curios;
+
+import com.refinedmods.refinedstorage2.platform.api.PlatformApi;
+import com.refinedmods.refinedstorage2.platform.common.util.IdentifierUtil;
+
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Mod(ModInitializer.ID)
+public final class ModInitializer {
+    public static final String ID = "refinedstorage_curios_integration";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ModInitializer.class);
+
+    public ModInitializer(final IEventBus eventBus) {
+        PlatformApi.INSTANCE.getSlotReferenceFactoryRegistry().register(
+            new ResourceLocation(ID, "curios"),
+            CuriosSlotReferenceFactory.INSTANCE
+        );
+        PlatformApi.INSTANCE.addSlotReferenceProvider(new CuriosSlotReferenceProvider());
+        LOGGER.info(
+            "Refined Storage - Curios Integration has loaded. RS2 ModId: {}",
+            IdentifierUtil.MOD_ID
+        );
+    }
+}
